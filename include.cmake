1fc96daf246b82fea8cb988fc49122be972c6d45
JBerger <JBerger@melco.com>
2017-04-04 17:20:28 -0600

    Added optional other target

--- include.cmake
+++ include.cmake
@@ -8,12 +8,17 @@ ENDIF()
 
 set(CGET_COPY_DEPENDENCIES_LOCAL_FILE "${CMAKE_CURRENT_LIST_DIR}/fix_dependencies.cmake" CACHE STRING "" FORCE)
 function(CGET_COPY_DEPENDENCIES_LOCAL target)
+    if("${ARGN}" STREQUAL "")
+        set(TARGET_FILE "$<TARGET_FILE:${target}>")
+    else()
+        set(TARGET_FILE "${ARGN}")
+    endif()
 	string(REPLACE ";" "\;" SAFE_PREFIX_PATH "${CMAKE_PREFIX_PATH}")
 		FILE(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${target}_install_script.cmake" "#Auto-generated don't modify\n")
 		INSTALL(SCRIPT "${CMAKE_CURRENT_BINARY_DIR}/${target}_install_script.cmake")
 		add_custom_command(TARGET ${target} POST_BUILD
 				COMMAND ${CMAKE_COMMAND} -DINSTALL_SCRIPT="${CMAKE_CURRENT_BINARY_DIR}/bundle_install.cmake" -DCMAKE_PREFIX_PATH="${SAFE_PREFIX_PATH}" "-DCMAKE_LIBRARY_PATH=${CMAKE_LIBRARY_PATH}"
-										 -DTARGET=\"$<TARGET_FILE:${target}>\" -DDIR="${CGET_INSTALL_DIR}/\;${CMAKE_LIBRARY_PATH}\;" -P "${CGET_COPY_DEPENDENCIES_LOCAL_FILE}"
+										 -DTARGET=\"${TARGET_FILE}\" -DDIR="${CGET_INSTALL_DIR}/\;${CMAKE_LIBRARY_PATH}\;" -P "${CGET_COPY_DEPENDENCIES_LOCAL_FILE}"
 				COMMENT "Fixing ${target}"
 		)
 endfunction()
